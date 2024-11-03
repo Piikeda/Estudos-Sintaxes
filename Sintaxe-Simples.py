fdb36d3e1543a1402bcd93a89fe3d1e93fd11b6a
Julio Cesar de Oliveira Ikeda <ikeda_tkd@hotmail.com>
2024-11-03 03:58:16 -0300

    Sintaxes-Simples-Teste

---
+++ Sintaxe-Simples.py
@@ -0,0 +1,30 @@
+
+
+vendas = 1000
+
+custo = 300
+
+lucro = vendas - custo
+
+print("O lucro foi de:" , lucro)
+
+vendas =2800
+
+if vendas > 1000:
+    print ('ganhou bonus de ' , vendas)
+else:
+     print('nao ganhou')
+
+listaprodutos = ["iphone","ipad", "macbook"]
+
+for produto in listaprodutos:
+    print(produto)
+    
+listaprecos = [1500,5000,5500]
+for item in listaprecos:
+   print(item)
+   print(item *2)
+ 
+ 
+for w in range(10):
+   print("sintaxes python simples")
